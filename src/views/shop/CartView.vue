dd03e9aa0aeaf5ab495d813ab079e360076080f4
platolinc <2686550623@qq.com>
2024-01-04 23:18:42 +0800

    使用computed完成数量和价格计算

--- src/views/shop/CartView.vue
+++ src/views/shop/CartView.vue
@@ -6,10 +6,10 @@
           src="http://www.dell-lee.com/imgs/vue3/basket.png"
           class="check__icon__img"
         />
-        <div class="check__icon__tag">1</div>
+        <div class="check__icon__tag">{{ total }}</div>
       </div>
       <div class="check__info">
-        总计：<span class="check__info__price">&yen;128</span>
+        总计：<span class="check__info__price">&yen; {{price}}</span>
       </div>
       <div class="check__btn">去结算</div>
     </div>
@@ -17,9 +17,50 @@
 </template>
 
 <script>
-// import { reactive, toRefs } from 'vue'
-// import { useRouter, useRoute } from 'vue-router'
-// import { get } from '../../utils/request'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+// 获取购物车信息逻辑
+const useCartEffect = () => {
+  const store = useStore()
+  const route = useRoute()
+  const shopId = route.params.id
+  const cartList = store.state.cartList
+
+  const total = computed(() => {
+    const productList = cartList[shopId]
+    let count = 0
+    if (productList) {
+      for (const i in productList) {
+        const product = productList[i]
+        count += product.count
+      }
+    }
+    return count
+  })
+
+  const price = computed(() => {
+    const productList = cartList[shopId]
+    let count = 0
+    if (productList) {
+      for (const i in productList) {
+        const product = productList[i]
+        count += product.count * product.price
+      }
+    }
+    return count.toFixed(2)
+  })
+  return { total, price }
+}
+
+export default {
+  name: 'CartView',
+  setup () {
+    const { total, price } = useCartEffect()
+    return { total, price }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -50,17 +91,19 @@
 
     &__tag {
       position: absolute;
-      right: .2rem;
+      left: .46rem;
       top: .04rem;
-      width: .2rem;
+      padding: 0 .04rem;
+      min-width: .2rem;
       height: .2rem;
       line-height: .2rem;
       background-color: $highlight-fontcolor;
-      border-radius: 50%;
+      border-radius: .1rem;
       font-size: .12rem;
       text-align: center;
       color: #FFF;
-      transform: scale(.5);
+      transform: scale(.5);//默认是从中心开始缩放
+      transform-origin: left center;//改为以左侧为中心缩放
     }
   }
 
