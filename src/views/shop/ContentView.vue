ab699bc4534344ecae47cbd2dc1ed12852f218cf
platolinc <2686550623@qq.com>
2024-01-10 00:08:12 +0800

    购物车上面的布局，能和列表的数据同步

--- src/views/shop/ContentView.vue
+++ src/views/shop/ContentView.vue
@@ -55,8 +55,8 @@
 <script>
 import { reactive, ref, toRefs, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
 import { get } from '../../utils/request'
+import { useCommonCartEffect } from './commonCartEffect'
 
 const categories = [
   { name: '全部商品', tab: 'all' },
@@ -91,16 +91,6 @@ const useCurrentListEffect = (currentTab, shopId) => {
   return { list }
 }
 
-// 购物车相关逻辑
-const useCartEffect = () => {
-  const store = useStore()
-  const { cartList } = toRefs(store.state)// store里的数据vuex会转换成reactive，所以必须用toRefs解构
-  const changeCartItemInfo = (shopId, productId, productInfo, num) => {
-    store.commit('changeCartItemInfo', { shopId, productId, productInfo, num })
-  }
-  return { cartList, changeCartItemInfo }
-}
-
 export default {
   name: 'ContentView',
   setup () {
@@ -108,7 +98,7 @@ export default {
     const shopId = route.params.id
     const { currentTab, handleTabClick } = useTabEffect()
     const { list } = useCurrentListEffect(currentTab, shopId)
-    const { cartList, changeCartItemInfo } = useCartEffect()
+    const { cartList, changeCartItemInfo } = useCommonCartEffect()
     return { list, currentTab, categories, handleTabClick, cartList, shopId, changeCartItemInfo }
   }
 }
